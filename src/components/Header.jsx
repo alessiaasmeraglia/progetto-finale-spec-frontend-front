8c003ee4cff40d9a28a1c4c2131fd0c0cbfae905
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 11:22:04 +0200

    Aggiunto contatore dei preferiti

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { useFavorites } from "../contexts/FavoritesContext";
 
 function Header() {
+    const { favorites } = useFavorites();
+
     return (
         <header>
             <nav>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/">
+                    Home
+                </NavLink>
 
-                <NavLink to="/shoes">Scarpette</NavLink>
+                <NavLink to="/shoes">
+                    Scarpette
+                </NavLink>
 
-                <NavLink to="/compare">Confronta</NavLink>
+                <NavLink to="/compare">
+                    Confronta
+                </NavLink>
 
-                <NavLink to="/favorites">Preferiti</NavLink>
+                <NavLink to="/favorites">
+                    Preferiti ({favorites.length})
+                </NavLink>
             </nav>
         </header>
     );

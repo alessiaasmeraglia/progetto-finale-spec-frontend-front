4141bcfd99a0a134934955cbcb21675f7330112e
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 10:29:51 +0200

    Aggiunto filtro categoria per la ricerca

--- src/pages/ShoesPage.jsx
+++ src/pages/ShoesPage.jsx
@@ -6,11 +6,12 @@ function ShoesPage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
     const [search, setSearch] = useState("");
+    const [category, setCategory] = useState("");
 
     const API_URL = import.meta.env.VITE_API_URL;
 
     useEffect(() => {
-        fetch(`${API_URL}/shoes?search=${search}`)
+        fetch(`${API_URL}/shoes?search=${search}&category=${category}`)
             .then((res) => {
                 if (!res.ok) {
                     throw new Error("Errore nel recupero delle scarpette");
@@ -27,7 +28,7 @@ function ShoesPage() {
             .finally(() => {
                 setLoading(false);
             });
-    }, [search, API_URL]);
+    }, [search, category, API_URL]);
 
     return (
         <section>
@@ -39,6 +40,17 @@ function ShoesPage() {
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
             />
+
+            <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+            >
+                <option value="">Tutte le categorie</option>
+                <option value="Boulder">Boulder</option>
+                <option value="Sport">Sport</option>
+                <option value="Indoor">Indoor</option>
+                <option value="All-round">All-round</option>
+            </select>
 
             {shoes.map((shoe) => (
                 <div key={shoe.id}>

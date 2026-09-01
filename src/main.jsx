254fc1f5b199416159808c2ab32642c317c28c11
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 14:50:57 +0200

    Installato bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.css";
+
+import App from "./App.jsx";
 import { FavoritesProvider } from "./contexts/FavoritesContext.jsx";
 import { CompareProvider } from "./contexts/CompareContext.jsx";
-import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

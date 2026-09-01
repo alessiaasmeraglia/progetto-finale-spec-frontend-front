46640553e0c0dd34c7f656e8d763d85fe67a197d
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 14:39:47 +0200

    Refactor preferiti

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
+import ShoeCard from "../components/ShoeCard";
 import { useFavorites } from "../contexts/FavoritesContext";
 
 function FavoritesPage() {
-    const { favorites, toggleFavorite } = useFavorites();
+    const { favorites } = useFavorites();
 
     if (favorites.length === 0) {
         return (
             <section>
                 <h1>Preferiti</h1>
+
                 <p>Non hai ancora aggiunto nessuna scarpetta ai preferiti.</p>
 
                 <Link to="/shoes">
@@ -22,18 +24,10 @@ function FavoritesPage() {
             <h1>Preferiti</h1>
 
             {favorites.map((shoe) => (
-                <div key={shoe.id}>
-                    <h2>{shoe.title}</h2>
-                    <p>{shoe.category}</p>
-
-                    <Link to={`/shoes/${shoe.id}`}>
-                        Dettagli
-                    </Link>
-
-                    <button onClick={() => toggleFavorite(shoe)}>
-                        Rimuovi dai preferiti
-                    </button>
-                </div>
+                <ShoeCard
+                    key={shoe.id}
+                    shoe={shoe}
+                />
             ))}
         </section>
     );

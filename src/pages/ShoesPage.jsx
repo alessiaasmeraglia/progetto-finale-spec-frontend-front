eb500f83686872b4afc58a10b8540d4347a06ef5
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 09:48:24 +0200

    Aggiunta fetch

--- src/pages/ShoesPage.jsx
+++ src/pages/ShoesPage.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+
 function ShoesPage() {
-    return <h1>Scarpette</h1>;
+    const [shoes, setShoes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        fetch(`${API_URL}/shoes`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Errore nel recupero delle scarpette");
+                }
+
+                return res.json();
+            })
+            .then((data) => {
+                setShoes(data);
+            })
+            .catch((err) => {
+                setError(err.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <section>
+            <h1>Scarpette</h1>
+
+            {shoes.map((shoe) => (
+                <div key={shoe.id}>
+                    <h2>{shoe.title}</h2>
+                    <p>{shoe.category}</p>
+                </div>
+            ))}
+        </section>
+    );
 }
 
 export default ShoesPage;

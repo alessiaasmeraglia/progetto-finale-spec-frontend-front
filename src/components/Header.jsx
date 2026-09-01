a9f84fe7247f765c95ea0001a50b5bc7c3ac4438
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-09-01 15:16:03 +0200

    Stile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,22 +8,75 @@ function Header() {
 
     return (
         <header>
-            <nav>
-                <NavLink to="/">
-                    Home
-                </NavLink>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+                <div className="container">
+                    <NavLink className="navbar-brand fw-bold" to="/">
+                        ClimbCompare
+                    </NavLink>
 
-                <NavLink to="/shoes">
-                    Scarpette
-                </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#mainNavbar"
+                        aria-controls="mainNavbar"
+                        aria-expanded="false"
+                        aria-label="Apri navigazione"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <NavLink to="/compare">
-                    Confronta ({compareItems.length})
-                </NavLink>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="mainNavbar"
+                    >
+                        <ul className="navbar-nav ms-auto align-items-lg-center gap-lg-2">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
 
-                <NavLink to="/favorites">
-                    Preferiti ({favorites.length})
-                </NavLink>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/shoes"
+                                >
+                                    Scarpette
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/compare"
+                                >
+                                    <i className="bi bi-arrow-left-right me-1"></i>
+                                    Confronta
+                                    <span className="badge text-bg-secondary ms-1">
+                                        {compareItems.length}
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/favorites"
+                                >
+                                    <i className="bi bi-heart me-1"></i>
+                                    Preferiti
+                                    <span className="badge text-bg-secondary ms-1">
+                                        {favorites.length}
+                                    </span>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </header>
     );
